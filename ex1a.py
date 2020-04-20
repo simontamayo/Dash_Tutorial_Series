93a14b87d0d7b23f0340f9a11d114b439043779a
simontamayo <simon.tamayo@quantumblack.com>
2020-04-20 18:02:37 -0500

    changes to fix errors

--- ex1a.py
+++ ex1a.py
@@ -8,28 +8,32 @@ import dash_html_components as html
 app = dash.Dash()
 
 app.layout = html.Div(
-    html.Div([
-        html.H1(children='Hello World'),
-
-        html.Div(children='''
+    html.Div(
+        [
+            html.H1(children="Hello World"),
+            html.Div(
+                children="""
             Dash: A web application framework for Python.
-        '''),
-
-        dcc.Graph(
-            id='example-graph',
-            figure={
-                'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            }
-        ),
-
-    ])
+        """
+            ),
+            dcc.Graph(
+                id="example-graph",
+                figure={
+                    "data": [
+                        {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
+                        {
+                            "x": [1, 2, 3],
+                            "y": [2, 4, 5],
+                            "type": "bar",
+                            "name": u"Montréal",
+                        },
+                    ],
+                    "layout": {"title": "Dash Data Visualization"},
+                },
+            ),
+        ]
+    )
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
